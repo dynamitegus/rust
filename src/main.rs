d9487131a2fcce2c082a620b3ba136b23e6b4b12
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2024-06-12 12:58:41 +1000

    gaming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("x in the small scope is {x}");
+    }
+    println!("x ounstide the scope is {x}");
 }
+      
