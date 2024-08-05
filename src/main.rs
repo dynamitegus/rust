99ec7b9cb7ff99efd1498291964a0f606d67e271
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2024-08-05 13:09:46 +1000

    if in a let statement

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    if number % 4 == 0 {
-        println!("number was divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number was divisible by 3");
-    } else if number % 2 == 0 {
-        println!("you get the point its divisible by 2");
-    } else {
-        println!("number is not divisible by 4 3 or 2");
-    }
+    println!("the value of number is {number}")
+
 }
