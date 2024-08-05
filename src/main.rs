e33826b7d9682f5c5d3ea57610d0b9f2198fc30a
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2024-08-05 13:00:06 +1000

    damn thats crazy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let number = 7;
 
-    if number {
-        println!("number was 3");
+    if number != 0{
+        println!("number was something that was infact, not zero");
     }
 }
