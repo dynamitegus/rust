58452cdc76eae6b07d46ed5b2f5fc962e1e8af00
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2024-08-05 13:06:50 +1000

    i maybe stupid

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ fn main() {
     let number = 6;
 
     if number % 4 == 0 {
-        println!("number was not divisible by 4");
+        println!("number was divisible by 4");
     } else if number % 3 == 0 {
-        println!("number was not divisible by 3");
+        println!("number was divisible by 3");
     } else if number % 2 == 0 {
-        println!("you get the point its no divisible by 2");
+        println!("you get the point its divisible by 2");
     } else {
         println!("number is not divisible by 4 3 or 2");
     }
