d034c40a80512d1e78e8300e5e060025614b0231
dynamitegus <69379880+dynamitegus@users.noreply.github.com>
2024-08-05 13:06:07 +1000

    we changing it up alot les goo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    let number = 7;
+    let number = 6;
 
-    if number != 0{
-        println!("number was something that was infact, not zero");
+    if number % 4 == 0 {
+        println!("number was not divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number was not divisible by 3");
+    } else if number % 2 == 0 {
+        println!("you get the point its no divisible by 2");
+    } else {
+        println!("number is not divisible by 4 3 or 2");
     }
 }
