e68747f939fea6529dbeb98e66a710f6c34a354b
dynamitegus <dynamitegus@dynamitegus.org>
2025-02-22 09:13:28 +1100

    it functions letsss goooooooooo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::io;
+
+
+fn fiban(x: String) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
+    let mut c: u128 = 0;
+
+    let n = x.trim().parse::<u128>().unwrap();
+if n == a {
+    return a;
+} else if n == b {
+    return b;
+}else {
+    for  _i in 2..n+1 {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+//define F0 and F1
+
+//let iterationcmdedition = std::env::args().nth(1).expect();
+
+//if iterationcmdedition != "" {
+  //  fiban(iterationcmdedition);
+//} 
+
+
+
+println!("Please input the iteration of the fibonacci sequence.");
+
+let mut iteration: String = String::new();
+
+io::stdin()
+    .read_line(&mut iteration)
+    .expect("Failed to read line");
+
+
+    println!("{}", fiban(iteration));
+
+
+
+
+
+
 }
